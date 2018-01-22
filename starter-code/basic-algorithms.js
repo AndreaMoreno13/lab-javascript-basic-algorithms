bce98d47efce99f3f781dd2f7fe3038f14073ba5
Andrea Moreno <andrea.moreno@gmail.com>
2018-01-22 15:13:16 -0500

    Implement code changes during class

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,44 @@
 // Names and Input
+console.log("I'm Ready!");
+var hacker1 = "Steve"
+console.log("The driver's name is " + hacker1);
+var hacker2 = prompt("What is the navigator's name?");
+console.log("The navigator's name is " + hacker2);
+if (hacker1.length>hacker2.length) { console.log('The driver has the longest name, it has ' + hacker1.length + " characters")}
+else if (hacker2.length > hacker1.length) { 
+  console.log('Yo navigator got the longest name, it has ' + hacker2.length + " characters")}
+else {
+  console.log("Wow! You both got equally long names," + hacker2.length + "characters")
+    }
+    
+var str ="";    
+for (i=0; i< hacker1.length; i++){
+  str += hacker1[i].toUpperCase() + " "}
+  console.log(str);
+  
+var str2 = "";
+for (j = hacker2.length -1; j>=0; j--){
+  str2 += hacker2[j];
+  }
+console.log(str2);
 
+if(hacker1 > hacker2){
+  console.log("The driver's name goes first")
+} else if (hacker1 < hacker2){
+  console.log("Yo, the navigator goes first definitely")
+} else {
+  console.log("What?! You both got the same name?")
+}
+
+var input =  prompt("Give me a string to check if it's a palindrome");
+var palindrome = "";
+
+for(k = input.length -1; k >=0; k-- ){
+  pal
+}
 
 //Conditionals
 
 
 // Lorem ipsum generator
+
